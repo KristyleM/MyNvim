3c18c21a82b9a158e18457ea5ffac54cec829bcc
KristyleM <17635675245@163.com>
2022-10-31 23:42:06 +0800

    Add rust plugin

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -284,6 +284,178 @@ local config = {
         event = "BufRead",
         config = function() require("goto-preview").setup {} end,
       },
+      ["simrat39/rust-tools.nvim"] = {
+        event = "BufRead",
+        config = function()
+          local rt = require "rust-tools"
+          local opts = {
+            tools = { -- rust-tools options
+
+              -- how to execute terminal commands
+              -- options right now: termopen / quickfix
+              executor = require("rust-tools/executors").termopen,
+
+              -- callback to execute once rust-analyzer is done initializing the workspace
+              -- The callback receives one parameter indicating the `health` of the server: "ok" | "warning" | "error"
+              on_initialized = nil,
+
+              -- automatically call RustReloadWorkspace when writing to a Cargo.toml file.
+              reload_workspace_from_cargo_toml = true,
+
+              -- These apply to the default RustSetInlayHints command
+              inlay_hints = {
+                -- automatically set inlay hints (type hints)
+                -- default: true
+                auto = true,
+
+                -- Only show inlay hints for the current li
+                only_current_line = false,
+
+                -- whether to show parameter hints with the inlay hints or not
+                -- default: true
+                show_parameter_hints = true,
+
+                -- prefix for parameter hints
+                -- default: "<-"
+                parameter_hints_prefix = "<- ",
+
+                -- prefix for all the other hints (type, chaining)
+                -- default: "=>"
+                other_hints_prefix = "=> ",
+                -- whether to align to the length of the longest line in the file
+                max_len_align = false,
+
+                -- padding from the left if max_len_align is true
+                max_len_align_padding = 1,
+
+                -- whether to align to the extreme right or not
+                right_align = false,
+
+                -- padding from the right if right_align is true
+                right_align_padding = 7,
+
+                -- The color of the hints
+                highlight = "Comment",
+              },
+
+              -- options same as lsp hover / vim.lsp.util.open_floating_preview()
+              hover_actions = {
+
+                -- the border that is used for the hover window
+                -- see vim.api.nvim_open_win()
+                border = {
+                  { "╭", "FloatBorder" },
+                  { "─", "FloatBorder" },
+                  { "╮", "FloatBorder" },
+                  { "│", "FloatBorder" },
+                  { "╯", "FloatBorder" },
+                  { "─", "FloatBorder" },
+                  { "╰", "FloatBorder" },
+                  { "│", "FloatBorder" },
+                },
+
+                -- whether the hover action window gets automatically focused
+                -- default: false
+                auto_focus = false,
+              },
+              -- settings for showing the crate graph based on graphviz and the dot
+              -- command
+              crate_graph = {
+                -- Backend used for displaying the graph
+                -- see: https://graphviz.org/docs/outputs/
+                -- default: x11
+                backend = "x11",
+                -- where to store the output, nil for no output stored (relative
+                -- path from pwd)
+                -- default: nil
+                output = nil,
+                -- true for all crates.io and external crates, false only the local
+                -- crates
+                -- default: true
+                full = true,
+
+                -- List of backends found on: https://graphviz.org/docs/outputs/
+                -- Is used for input validation and autocompletion
+                -- Last updated: 2021-08-26
+                enabled_graphviz_backends = {
+                  "bmp",
+                  "cgimage",
+                  "canon",
+                  "dot",
+                  "gv",
+                  "xdot",
+                  "xdot1.2",
+                  "xdot1.4",
+                  "eps",
+                  "exr",
+                  "fig",
+                  "gd",
+                  "gd2",
+                  "gif",
+                  "gtk",
+                  "ico",
+                  "cmap",
+                  "ismap",
+                  "imap",
+                  "cmapx",
+                  "imap_np",
+                  "cmapx_np",
+                  "jpg",
+                  "jpeg",
+                  "jpe",
+                  "jp2",
+                  "json",
+                  "json0",
+                  "dot_json",
+                  "xdot_json",
+                  "pdf",
+                  "pic",
+                  "pct",
+                  "pict",
+                  "plain",
+                  "plain-ext",
+                  "png",
+                  "pov",
+                  "ps",
+                  "ps2",
+                  "psd",
+                  "sgi",
+                  "svg",
+                  "svgz",
+                  "tga",
+                  "tiff",
+                  "tif",
+                  "tk",
+                  "vml",
+                  "vmlz",
+                  "wbmp",
+                  "webp",
+                  "xlib",
+                  "x11",
+                },
+              },
+            },
+            -- all the opts to send to nvim-lspconfig
+            -- these override the defaults set by rust-tools.nvim
+            -- see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#rust_analyzer
+            server = {
+              -- standalone file support
+              -- setting it to false may improve startup time
+              standalone = true,
+            }, -- rust-analyzer options
+
+            -- debugging stuff
+            -- dap = {
+            --   adapter = {
+            --     type = "executable",
+            --     command = "lldb-vscode",
+            --     name = "rt_lldb",
+            --   },
+            -- },
+          }
+          require("rust-tools").setup(opts)
+        end,
+      },
       -- We also support a key value style plugin definition similar to NvChad:
       -- ["ray-x/lsp_signature.nvim"] = {
       --   event = "BufRead",
@@ -332,7 +504,7 @@ local config = {
     },
     -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
     ["mason-null-ls"] = { -- overrides `require("mason-null-ls").setup(...)`
-      ensure_installed = { "prettier", "stylua", "black", "gofmt", "yamlfmt"},
+      ensure_installed = { "prettier", "stylua", "black", "gofmt", "yamlfmt" },
     },
   },
 
@@ -397,15 +569,14 @@ local config = {
 }
 
 -- Copy from https://github.com/nvim-telescope/telescope.nvim/issues/559
--- and I remove zM here to avoid folding whole file. 
-vim.api.nvim_create_autocmd('BufRead', {
-   callback = function()
-      vim.api.nvim_create_autocmd('BufWinEnter', {
-         once = true,
-         command = 'normal! zx'
-      })
-   end
+-- and I remove zM here to avoid folding whole file.
+vim.api.nvim_create_autocmd("BufRead", {
+  callback = function()
+    vim.api.nvim_create_autocmd("BufWinEnter", {
+      once = true,
+      command = "normal! zx",
+    })
+  end,
 })
 
 return config
-
