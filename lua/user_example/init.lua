AstroNvim/AstroNvim

chore(user_example): add default lsp format timeout

lua/user_example/init.lua
@@ -140,6 +140,7 @@ local config = {
       disabled = { -- disable formatting capabilities for the listed clients
         -- "sumneko_lua",
       },
+      time_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
       --   return true
       -- end

KristyleM/MyNvim

lua/user_example/init.lua
@@ -140,6 +140,7 @@ local config = {
       disabled = { -- disable formatting capabilities for the listed clients
         -- "sumneko_lua",
       },
+      timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
       --   return true
       -- end